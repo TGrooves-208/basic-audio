1855f924c1fbf8e78f6ee1246af6b19be860dc2a
TGrooves-208 <tgrooves.208@gmail.com>
2016-01-17 18:35:41 -0700

    Added two buttons a slider and an audio file from Piano Society

--- working-with-audio/ViewController.swift
+++ working-with-audio/ViewController.swift
@@ -1,18 +1,58 @@
 //
 //  ViewController.swift
 //  working-with-audio
-//
+//  Downloaded an audio sample from Piano Society
+//  Added a two buttons (Play, Pause)
+//  Added a slider for the volume control
 //  Created by Gil Aguilar on 1/17/16.
 //  Copyright © 2016 redeye-dev. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var slider: UISlider!
+    
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        player.play()
+        
+    }
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        player.pause()
+        
+    }
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+        
+        player.volume = slider.value
+        
+    }
+    
+        var player: AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")!
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath:audioPath))
+            
+            
+        } catch {
+            
+        }
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
